d277af4f91af46da5445af0fcd61678bbbcb8195
AlexDiz <alexdiz88@gmail.com>
2023-06-07 18:36:00 +0200

    first commit

---
+++ src/main/java/de/aittr/bd1/entity/Address.java
@@ -0,0 +1,26 @@
+package de.aittr.bd1.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String country;
+    private String city;
+    private String street;
+    private Integer houseNumber;
+
+    @ManyToOne
+    private Client client;
+}
